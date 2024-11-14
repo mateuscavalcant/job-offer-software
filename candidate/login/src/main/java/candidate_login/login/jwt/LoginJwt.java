de456b8342ba2a133f6347091e98ac6364f991ba
mateuscavalcant <mateuscavalcant7@gmail.com>
2024-11-14 07:25:21 -0300

    Update extractUsername to extractEmail

--- candidate/login/src/main/java/candidate_login/login/jwt/LoginJwt.java
+++ candidate/login/src/main/java/candidate_login/login/jwt/LoginJwt.java
@@ -11,16 +11,16 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class LoginJwt {
     private String secretKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ1c3VhcmlvX2V4ZW1wbG8iLCJlbWFpbCI6InVzdWFyaW9AZXhlbXBsby5jb20iLCJpYXQiOjE2MjY2NjY2NjYsImV4cCI6MTYyNjY3MDI2Nn0.4fP3x8y8y8y8y8y8y8y8y8y8y8y8y8y8y8y8y8y8\r\n";
 
-    public String generateToken(String username) {
+    public String generateToken(String email) {
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
-    public String extractUsername(String token) {
+    public String extractEmail(String token) {
         return Jwts.parser()
                 .setSigningKey(secretKey)
                 .parseClaimsJws(token)
@@ -28,8 +28,8 @@ public class LoginJwt {
                 .getSubject();
     }
 
-    public boolean validateToken(String token, String username) {
-        return (username.equals(extractUsername(token)) && !isTokenExpired(token));
+    public boolean validateToken(String token, String email) {
+        return (email.equals(extractEmail(token)) && !isTokenExpired(token));
     }
 
     private boolean isTokenExpired(String token) {
