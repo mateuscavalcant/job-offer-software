5a7f5da0b81c44e488f4ef1fdc92c32cf8457806
mateuscavalcant <mateuscavalcant7@gmail.com>
2024-11-21 09:00:49 -0300

    Remove Enterprise Folder

--- enterprise/signup/src/main/java/enterprise_signup/signup/repository/UpdateEnterpriseRepository.java
+++
@@ -1,9 +0,0 @@
-package enterprise_signup.signup.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import enterprise_signup.signup.model.UpdateEnterpriseModel;
-
-public interface UpdateEnterpriseRepository extends JpaRepository<UpdateEnterpriseModel, Long> {
-    UpdateEnterpriseModel finById(long id);
-}
