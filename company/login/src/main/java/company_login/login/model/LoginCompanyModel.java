a8b9c415a52d6bb5fa34eaac9c9bffa558ef51dd
mateuscavalcant <mateuscavalcant7@gmail.com>
2024-11-22 07:58:35 -0300

    Add Login Company Model

---
+++ company/login/src/main/java/company_login/login/model/LoginCompanyModel.java
@@ -0,0 +1,58 @@
+package company_login.login.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="company")
+public class LoginCompanyModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+
+    public LoginCompanyModel() {}
+
+
+    public LoginCompanyModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password =  password;
+    }
+
+    
+}
